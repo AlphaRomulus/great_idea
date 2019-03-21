efa42b82ebfa2a0e34c5b2d93438e60e0bace9fc
AlphaRomulus <rjordan1352@gmail.com>
2019-03-21 15:42:41 -0700

    Style Fix

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,4 @@
 <footer>
-		
 		<h2>Contact</h2>
 		<address>
 			123 Way 456 Street<br>
@@ -7,8 +6,7 @@
 		</address>
 		<a href="">1 (888) 888-8888</a>
 		<a href="">sales@greatidea.io</a>
-
 		<p>Copyright Great Idea! 2018</p>
-  
+	</footer>
   </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -7,158 +7,17 @@
   <title>Great Idea!</title>
 
   <link href="https://fonts.googleapis.com/css?family=Bangers|Titillium+Web" rel="stylesheet">
+  
 
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
   
-  <style>
-	/* http://meyerweb.com/eric/tools/css/reset/ 
-	v2.0 | 20110126
-	License: none (public domain)
-		*/
-	
-	html, body, div, span, applet, object, iframe,
-	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-	a, abbr, acronym, address, big, cite, code,
-	del, dfn, em, img, ins, kbd, q, s, samp,
-	small, strike, strong, sub, sup, tt, var,
-	b, u, i, center,
-	dl, dt, dd, ol, ul, li,
-	fieldset, form, label, legend,
-	table, caption, tbody, tfoot, thead, tr, th, td,
-	article, aside, canvas, details, embed, 
-	figure, figcaption, footer, header, hgroup, 
-	menu, nav, output, ruby, section, summary,
-	time, mark, audio, video {
-		margin: 0;
-		padding: 0;
-		border: 0;
-		font-size: 100%;
-		font: inherit;
-		vertical-align: baseline;
-	}
-	/* HTML5 display-role reset for older browsers */
-	article, aside, details, figcaption, figure, 
-	footer, header, hgroup, menu, nav, section {
-		display: block;
-	}
-	body {
-		line-height: 1;
-	}
-	ol, ul {
-		list-style: none;
-	}
-	blockquote, q {
-		quotes: none;
-	}
-	blockquote:before, blockquote:after,
-	q:before, q:after {
-		content: '';
-		content: none;
-	}
-	table {
-		border-collapse: collapse;
-		border-spacing: 0;
-	}
-	html, body {
-		height: 100%;
-		font-family: 'Titillium Web', sans-serif;
-	}
-	h1, h2, h3, h4, h5 {
-		font-family: 'Bangers', cursive;
-		letter-spacing: 1px;
-		margin-bottom: 15px;
-	}
-	/* Your code starts here! */
-    *{
-    box-sizing:border-box;
-    }
-    p{
-  	line-height:1.25;
-    }
-    header,main,section,footer, span{
-  	width:875px;
-  	margin:auto;
-    }
-    header{
-  	position:fixed;
-	top:0;
-  	min-width:875px;
-	width:100%;
-	padding:20px;
-  	margin:0 auto;
-  	background:#fff;
-    }
-    header nav{
-  	display:inline-block;
-  	margin-top:5px;
-    }
-    header nav a{
-  	text-decoration:none;
-  	color:grey;
-  	margin:20px;
-    }
-    header img{
-  	float:right;
-    }
-    main{
-  	padding:40px 0 0 0;
-  	margin-top:40px;
-    }
-    main div{
-  	display:inline-block;
-  	width:49.5%;
-  	padding:30px;
-  	text-align:center;
-    }
-    main h1{
-  	font-size:5rem;
-    }
-    main div button{
-  	border:1px solid black;
-  	background-color:transparent;
-  	padding:5px 20px;
-  	font-size:1.25rem;
-    }
-    main div img{
-  	width:100%;
-    }
-    span{
-  	border: 1px solid #222;
-  	display:block;
-  	margin:20px auto;
-    }
-    section h2, footer h2{
-  	font-weight:2;
-    }
-    
-    footer a{
-  	text-decoration:none;
-  	color:#000;
-  	display:block;
-  	margin-top:20px;
-    }
-    footer p{
-  	text-align:center;
-    }
-    .col-6{
-  	margin:40px 0;
-  	display:inline-block;
-  	width:49.5%;
-  	padding:5px;
-    }
-    .col-4{
-      margin:40px 0;
-  	display:inline-block;
-  	width:32.5%;
-  	padding:5px;
-    }
-  </style>
-</head>
+  <?php include('css/style.html');?>
 
+</head>
   <body>
-  <header>
+	<header>
 		<nav>
 			<a href="?page=services">Services</a>
 			<a href="?page=product">Product</a>
